c6802d6a23996c34b7e9cb5d0d7649f45a5b4462
Michael Krämer <info.m.kraemer@gmail.com>
2024-09-05 21:08:18 +0200

    background and start image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StartScreenComponent } from './start-screen/start-screen.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StartScreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
+
 export class AppComponent {
   title = 'ringoffire';
 }
